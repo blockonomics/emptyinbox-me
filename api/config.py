46e6d232ef9e9448a3c3bbb99c9dd316860db525
cnohall <christopher.nohall@gmail.com>
2025-08-13 18:58:17 +0900

    restore config.py

--- api/config.py
+++ api/config.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/emptyinbox.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emptyinbox.db'
 db = SQLAlchemy(app)
