3cf1e48fb88137504b8a288dd9bcf83f2ffd7940
cnohall <christopher.nohall@gmail.com>
2025-08-19 18:07:50 +0900

    add styles

--- static/components/atoms/CreateInboxButton/index.js
+++ static/components/atoms/CreateInboxButton/index.js
@@ -1,7 +1,7 @@
 import { createElement } from "../../../utils/domHelpers.js";
 
 export function createCreateInboxButton() {
-  const button = createElement('button', 'create-inbox-btn');
+  const button = createElement('button', 'create-inbox-btn container');
   button.title = 'Create new inbox';
   button.innerHTML = `
     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" style="margin-right: 8px;">
