d10d9fe03ea28ceb71afd58796acc3b79faeb73a
cnohall <christopher.nohall@gmail.com>
2025-08-19 15:26:00 +0900

    Update createdDate

--- static/components/molecules/InboxPreview/index.js
+++ static/components/molecules/InboxPreview/index.js
@@ -3,13 +3,13 @@ import { createElement } from "../../../utils/domHelpers.js";
 export function createInboxPreview(inbox) {
   const container = createElement('div', 'inbox-preview');
 
-  // const createdDate = new Date(inbox.created_at * 1000).toLocaleString('en-US', {
-  //   day: '2-digit',
-  //   month: 'short',
-  //   year: 'numeric',
-  //   hour: '2-digit',
-  //   minute: '2-digit',
-  // });
+  const createdDate = new Date(inbox.created_at * 1000).toLocaleString('en-US', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 
   container.innerHTML = `
     <div class="inbox-header">
@@ -22,7 +22,7 @@ export function createInboxPreview(inbox) {
           </svg>
         </button>
       </div>
-      <div class="inbox-created">Created: ${'N/A'}</div>
+      <div class="inbox-created">Created: ${createdDate}</div>
     </div>
   `;
 
