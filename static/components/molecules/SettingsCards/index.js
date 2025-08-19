54cc4c1b42bc736278eeacd08db939230512e692
cnohall <christopher.nohall@gmail.com>
2025-08-19 14:18:43 +0900

    update billing

--- static/components/molecules/SettingsCards/index.js
+++ static/components/molecules/SettingsCards/index.js
@@ -4,10 +4,15 @@ import { createApiKeyCard } from "../../organisms/ApiKeyCard/index.js";
 
 export function renderSettingsCards() {
   const statsContainer = createElement('div', 'messages-stats');
-  
+
+  // Create billing content container
+  const billingContent = createElement('div', 'billing-text');
+  billingContent.id = 'billing-transactions';
+  billingContent.textContent = 'Still need to be implemented'; // Optional placeholder
+
   // Add all stat cards
   statsContainer.appendChild(createApiKeyCard());
-  statsContainer.appendChild(createStatCard('Billing Transactions', 'Still need to be implemented', 'billing-text'));
-  
+  statsContainer.appendChild(createStatCard('Billing Transactions', billingContent));
+
   return statsContainer;
 }
